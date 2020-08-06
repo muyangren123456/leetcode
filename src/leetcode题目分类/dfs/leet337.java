5d04540f04603e7dbf1114129a51e1faee946d80
darren <darren_gao@hf.acmlife.org>
2020-08-06 09:53:32 +0800

    添加了dfs

---
+++ src/leetcode题目分类/dfs/leet337.java
@@ -0,0 +1,46 @@
+package leetcode题目分类.dfs;
+
+import leetcode题目分类.treenode.treeNode;
+
+import java.util.*;
+
+public class leet337 {
+    /*
+在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。
+这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。
+一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房
+子在同一天晚上被打劫，房屋将自动报警。
+
+   */
+    Map<treeNode,Integer> f = new HashMap<treeNode,Integer>();
+    Map<treeNode,Integer> g = new HashMap<treeNode,Integer>();
+    public static void main(String[] args) {
+        leet337 leet337 = new leet337();
+        List<treeNode> nodelist = new ArrayList<treeNode>();
+        Integer[] num = {3,2,3,null,3,null,1};
+        for(Integer a : num){
+            if (a != null){
+                nodelist.add(new treeNode(a));
+            }
+            else{
+                nodelist.add(new treeNode(0));
+            }
+        }
+        System.out.println(leet337.rob(nodelist.get(0)));
+    }
+
+    public int rob(treeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root,0),g.getOrDefault(root,0));
+    }
+    public void dfs(treeNode node){
+        if(node == null){
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node,node.val+g.getOrDefault(node.left,0)+g.getOrDefault(node.right,0));
+        g.put(node,Math.max(f.getOrDefault(node.left,0),g.getOrDefault(node.left,0))+
+        Math.max(f.getOrDefault(node.right,0),g.getOrDefault(node.right,0)));
+    }
+}
---
+++ src/leetcode题目分类/treenode/treeNode.java
@@ -0,0 +1,10 @@
+package leetcode题目分类.treenode;
+
+public class treeNode {
+    public int val;
+    public treeNode left;
+    public treeNode right;
+    public treeNode(int x ){
+        val =x;
+    }
+}
